5c674267cd41424c6fab8193fe29b5eaf34733fa
josephvelazcordz <josephvelazco@gmail.com>
2022-11-18 09:56:23 +0100

    TD5 PDLA

---
+++ TD5PDLA/src/main/java/LoginVerify.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginVerify
+ */
+public class LoginVerify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginVerify() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("index.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("uname");
+		String pwd=request.getParameter("password");
+		if(uname.equals("admin") && pwd.equals("admin"))
+		{
+			System.out.println("Les identifiants sont correctes");
+		}
+		else
+		{
+			System.out.println("Erreur dans les identifiants");
+		}
+				
+		doGet(request, response);
+	}
+
+}
---
+++ TD5PDLA/src/main/webapp/index.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"> 
+<title>TD5</title>
+</head>
+<body>
+	<form method="post" action="check.do">
+		<table>
+			<tr>
+				<td>Login</td>
+				<td><input type="text" name="uname"></td>
+			</tr>
+			<tr>
+				<td>PWD</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="login"></td>
+			</tr>
+		
+		</table>
+	</form>
+
+</body>
+</html>
